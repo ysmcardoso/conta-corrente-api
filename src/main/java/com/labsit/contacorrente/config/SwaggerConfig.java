4d2cb1c83d2d490c8e2a1ad5618d2e17e7394cbd
Yasmin Cardoso <yasmin.cardoso@uds.com.br>
2020-11-14 16:54:32 -0300

    Modificação da configuração do swagger.

--- src/main/java/com/labsit/contacorrente/config/SwaggerConfig.java
+++ src/main/java/com/labsit/contacorrente/config/SwaggerConfig.java
@@ -2,8 +2,6 @@ package com.labsit.contacorrente.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -13,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {
 
     @Bean
     public Docket productApi() {
@@ -32,13 +30,5 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
                 .build();
-    }
-
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
